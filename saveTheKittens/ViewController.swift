f0b2bfaf3cbbaa33bc000022de952876a6817927
erkutbas <ebas1@binghamton.edu>
2017-10-18 00:17:01 +0300

    Initial Commit

---
+++ saveTheKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  saveTheKittens
+//
+//  Created by Erkut Baş on 18/10/2017.
+//  Copyright © 2017 Erkut Baş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
